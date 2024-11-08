2cb0cada8b7a70d5e8676cbf3b74237c53fcdd4e
Creative-Md-Arif <mohammadarif.ma02@gmail.com>
2024-11-08 12:42:36 +0600

    Project Stracture Create and database Connect

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+// console.log("hello world");
+
+
+
+// pakages
+
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+
+// utils
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+// connect to database
+connectDB();
+
+// create express app
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// routes
